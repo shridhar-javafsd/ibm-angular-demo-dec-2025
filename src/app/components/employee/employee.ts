377168619b82faaaa0002830975d475ce0ce1142
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-13 15:36:51 +0530

    angular demo

--- src/app/components/employee/employee.ts
+++ src/app/components/employee/employee.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Department } from '../department/department';
+
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [RouterOutlet, CommonModule, Department],
   templateUrl: './employee.html',
   styleUrl: './employee.css',
 })
 export class Employee {
-  data = { id: 101, name: 'Sonu', salary: 10.50 }
-
+  empData = { id: 101, name: 'Sonu', salary: 10.50 }
 }
 
 
@@ -19,9 +23,6 @@ export class Employee {
 //   template: `<p>this</p>`
 // })
 // export class Employee {
-
-//   data = { id: 101, name: 'Sonu', salary: 10.50 }
-
 // }
 
 
