01044d1c1c991e26696bb8116c64e7c1cda84886
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-13 15:22:44 +0530

    angular demo

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,12 +4,19 @@ import { Employee } from './components/employee/employee';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Employee],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
   protected readonly title = signal('ibm-angular-demo');
+
+  firstName: string = 'Sonu';
+
 }
 
+
+// data exchange bw component and template -
+// component to template =
+// 1.  {{ string interpolation }}
+// 2, 3, 4,
--- src/app/components/employee/employee.ts
+++ src/app/components/employee/employee.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './employee.css',
 })
 export class Employee {
+  data = { id: 101, name: 'Sonu', salary: 10.50 }
 
 }
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-employee',
+//   template: `<p>this</p>`
+// })
+// export class Employee {
+
+//   data = { id: 101, name: 'Sonu', salary: 10.50 }
+
+// }
+
 
