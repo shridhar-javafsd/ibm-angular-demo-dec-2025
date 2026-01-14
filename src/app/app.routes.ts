1b2134a453957a13935476f5f008ad18a7dcfa1e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-14 16:44:02 +0530

    angular fundas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Employee } from './components/employee/employee';
+import { Department } from './components/department/department';
+import { User } from './components/user/user';
+import { Home } from './components/home/home';
+import { Page404 } from './page404/page404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'employee', component: Employee },
+    { path: 'department', component: Department },
+    { path: 'user', component: User },
+    { path: '**', component: Page404 }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Employee } from './components/employee/employee';
-import { Parent } from './components/parent/parent';
-import { User } from './components/user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Employee, Parent, User],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
